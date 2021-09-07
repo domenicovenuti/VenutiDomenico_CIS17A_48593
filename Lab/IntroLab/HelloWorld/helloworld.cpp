8db383463af5697921bb8dbc55a23c83ae9f90e2
domenicovenuti <domenicovenuti@outlook.com>
2021-09-06 17:12:36 -0700

    Signed-off-by: domenicovenuti <domenicovenuti@outlook.com>

---
+++ Lab/IntroLab/HelloWorld/helloworld.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File: helloworld.cpp
+ * Author: Domenico Venuti
+ * Created on September 6, 2021 - Assignment_1, 4:00 PM
+ * Purpose:  CIS-17A - 48591 - IntroLab (HelloWorld Program)
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"HelloWord";
+    return 0;
+}
+
--- Lab/README.md
+++
@@ -1,2 +0,0 @@
-# VenutiDomenico_CIS17A_48593
-Programming: Objects RCC Fall 2021
